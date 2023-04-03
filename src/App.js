2a972c145c111f00564d1f167d22004561968212
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-04-04 00:07:12 +0530

    Add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginPage from "./pages/Login";
+import Verify from "./pages/Verify";
+import Error from "./pages/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path:"/",
+      element: <LoginPage />,
+      errorElement: <Error />,
+    },
+    {
+      path: "/verify",
+      element: <Verify />
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <RouterProvider router={appRouter} />
+    </>
   );
 }
 
