beebae0e32117bb100f5a160e458440c2aa96791
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-04-04 21:57:21 +0530

    Check email with previous email

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,49 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { addItem } from "../utils/emailSlice";
+import { addItem as otpAddItem } from "../utils/otpSlice";
 
 const LoginPage = () => {
   const [userEmail, setUserEmail] = useState("");
+  const [userOTP, setUserOTP] = useState();
+  const [toggleAPI, setToggleAPI] = useState(false);
+  const [emailCollision, setEmailCollision] = useState(false);
   const dispatch = useDispatch();
+  const prevEmail = useSelector((store) => store.email.items);
 
-  const handleEmailChange = (e) => {
-    setUserEmail(e.target.value);
-  };
+  useEffect(() => {
+    // Make a dummy API call to send an OTP to the email.
+    const sendOTP = async (emailId) => {
+      const data = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          email: emailId,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      const response = await data.json();
+      // setUserOTP(response.data.otp)
+      setUserOTP(1234);
+    };
+    sendOTP(userEmail);
+  }, [toggleAPI]);
+
+  useEffect(() => {
+    // Check if the email is similar to the previous email.
+    if (prevEmail.length > 0 && prevEmail[0] == userEmail) {
+      setEmailCollision(true);
+    } else {
+      setEmailCollision(false);
+    }
+  }, [userEmail]);
 
   const onSubmitHandler = () => {
+    setToggleAPI(toggleAPI ? false : true);
     dispatch(addItem(userEmail));
+    userOTP && dispatch(otpAddItem(userOTP));
   };
 
   return (
@@ -24,15 +55,18 @@ const LoginPage = () => {
         name="user-email"
         id="user-email"
         value={userEmail}
-        onChange={handleEmailChange}
+        onChange={e => setUserEmail(e.target.value)}
       />
+      {emailCollision ? (
+        <label>Previous email matches the current email!</label>
+      ) : null}
       <Link to="/verify">
-      <button
-        className="text-white bg-sky-500 hover:bg-sky-700 rounded-lg text-center h-10 mt-4 ml-16 w-24"
-        onClick={onSubmitHandler}
-      >
-        Send OTP
-      </button>
+        <button
+          className="text-white bg-sky-500 hover:bg-sky-700 rounded-lg text-center h-10 mt-4 ml-16 w-24"
+          onClick={onSubmitHandler}
+        >
+          Send OTP
+        </button>
       </Link>
     </div>
   );
