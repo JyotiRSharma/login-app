e85cdb0d2fcab89c7457ec750855f5271b3d6ea2
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-04-04 21:57:51 +0530

    Get opt in verify component

--- src/components/Verify.jsx
+++ src/components/Verify.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import store from "../utils/store";
 
 const Verify = () => {
   const email = useSelector(store => store.email.items);
+  const otp = useSelector(store => store.otp.items);
+
   return (
     <>
-    <label>Verify page works! {email[0]}</label>
+    <label>Verify page works! {[email[0], otp[0]]}</label>
     </>
   )
 }
