9c47e07daa2626ece6bd010e3e6f227f2d299a0b
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-04-04 21:58:06 +0530

    Add OTP slice

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import emailSlice from "./emailSlice";
+import otpSlice from "./otpSlice";
 
 const store = configureStore({
   reducer: {
     email: emailSlice,
+    otp: otpSlice,
   }
 });
 
