d5b9f675992b84c45e563cd4fdc3410f9cf7b1ad
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-04-06 02:36:51 +0530

    Update

--- src/components/dialog/dialog.jsx
+++ src/components/dialog/dialog.jsx
@@ -4,8 +4,8 @@ const CustomDialog = ({ isShowing, hide, direction, ...props }) => {
   return isShowing
     ? createPortal(
         <div className="grid place-content-center fixed top-0 left-0 w-screen h-screen z-{1040} bg-gray-100 font-mono">
-          <button onClick={hide}>Close</button>
           {props.children}
+          <button className="absolute" onClick={hide}>Close</button>
         </div>,
         document.body
       )
