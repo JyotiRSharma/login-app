1210422785dc4ee6e5ca9cef09ee15ed74281ff3
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-04-06 02:45:33 +0530

    Add red close text

--- src/components/dialog/dialog.jsx
+++ src/components/dialog/dialog.jsx
@@ -5,7 +5,7 @@ const CustomDialog = ({ isShowing, hide, direction, ...props }) => {
     ? createPortal(
         <div className="grid place-content-center fixed top-0 left-0 w-screen h-screen z-{1040} bg-gray-100 font-mono">
           {props.children}
-          <button className="absolute" onClick={hide}>Close</button>
+          <button className="absolute right-10 top-10 text-lg text-red-700" onClick={hide}>Close</button>
         </div>,
         document.body
       )
